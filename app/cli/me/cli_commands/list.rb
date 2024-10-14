a150f6608deabaa0566089771e4fd42b9e9fb735
doooby <zelazk.o@email.cz>
2024-10-14 23:43:07 +1300

    me6 listing options

--- app/cli/me/cli_commands/list.rb
+++ app/cli/me/cli_commands/list.rb
@@ -1,12 +1,31 @@
 class Me::CliCommands::List < Me::CommandBase
-  # attr_accessor :task_name, :text
+  KNOWN_COLUMNS = %i[ index id task start end text ]
 
   def setup_parser
     parser.banner = "Usage: me list [OPTIONS]".blue
+
+    parser.on("-cLIST", String, "columns to show (default: #{KNOWN_COLUMNS.join ","})") do |list|
+      list = list.split(",").map(&:to_sym)
+      @show_columns = KNOWN_COLUMNS && list
+    end
+
+    parser.on("-m", String, "minimize output") do |value|
+      @minimize_output = true
+    end
+
+    parser.on("-fCOL=VAL", String, "filter recrods (e.g. -fid=1)") do |value|
+      @filters ||= {}
+      result = value.match %r{^(\w+)=(.+)$}
+      unless result
+        raise "invalid filter `#{value}`"
+      end
+      @filters[result[1]] = result[2]
+    end
   end
 
   def parse! args
     _ = parser.parse args
+    puts @filters.inspect
     #     @task_name, @text, *_ = parser.parse args
     #     if task_name.blank?
     #       log :out, <<-DOC
@@ -21,44 +40,31 @@ class Me::CliCommands::List < Me::CommandBase
     scope = scope.order id: :desc
     scope = scope.limit 10
     records = scope.to_a
+    return if records.length.zero?
 
-    rows = [
-      %w[ row ID task start end text ]
-    ]
-    records.each_with_index do |record, index|
-      rows.push [
-        (-1 * (index + 1)).to_s,
-        record.id.to_s,
-        record.task.to_s,
-        record.start_at.to_s,
-        record.end_at.to_s,
-        record.text.to_s
-      ]
-    end
-
-    print_table rows, true unless rows.first.blank?
+    columns = @show_columns || KNOWN_COLUMNS
+    table = Me::CliCommands::List.tasks_to_table records, columns
+    table.minimized = true if @minimize_output
+    table.each_text_row{ log :out, _1 }
   end
 
-  def print_table rows, header
-    sizes = Array.new(rows.first.length) { 0 }
-    rows.each_with_index do |row, row_index|
-      sizes.length.times do |column_index|
-        cell = row[column_index]
-        puts "cell[#{row_index}:#{column_index}] #{cell}"
-        sizes[column_index] = cell.length if cell.length > sizes[column_index]
+  def self.tasks_to_table records, columns
+    rows = records.map.with_index do |record, record_index|
+      columns.map.with_index do |column, column_index|
+        case column
+        when :index then (- record_index) - 1
+        when :id then record.id
+        when :task then record.task.to_s
+        when :start then format_time record.start_at
+        when :end then format_time record.end_at
+        end
       end
     end
-    rows.each_with_index do |row, row_index|
-      items = []
-      sizes.each_with_index do |column_size, column_index|
-        text = row[column_index]
-        padd = column_size - text.length
-        items.push text + (" " * padd)
-      end
-      row_text = items.join(
-        header && row_index.zero? ? " | " : "   "
-      )
-      puts row_text
-    end
+    Me::Terminal::DataTable.new rows, columns
+  end
+
+  def self.format_time time
+    return "" unless time
+    time.localtime.strftime "%y%m%d:%H:%M"
   end
 end
---
+++ app/cli/me/terminal.rb
@@ -0,0 +1,54 @@
+module Me::Terminal
+  class DataTable
+    attr_reader :rows, :columns
+    attr_accessor :minimized
+
+    def initialize rows, columns
+      @rows = rows.each do |row|
+        row.map!{ _1.to_s }
+      end
+      @columns = columns.map!{ _1.to_s }
+    end
+
+    def each_text_row
+      column_sizes = columns.map.with_index do |column, index|
+        items_max = rows.map{ _1[index].length }.max
+        [ items_max, minimized ? 0 : column.length ].max
+      end
+
+      unless minimized
+        header = String.new "│ "
+        columns.each_with_index do |text, index|
+          header << " │ " unless index.zero?
+          Me::Terminal.push_cell_into! header, text, column_sizes[index]
+        end
+        header << " │"
+        yield header
+
+        header_separator = String.new "├"
+        chars = 1 + column_sizes.sum + (column_sizes.length * 3) - 1
+        header_separator << ("-"  * chars)
+        yield header_separator
+
+      end
+
+      rows.each do |row|
+        text = minimized ? String.new : String.new("│ ")
+        separator = minimized ? " " : "   "
+        columns.length.times do |index|
+          text << separator unless index.zero?
+          Me::Terminal.push_cell_into! text, row[index], column_sizes[index]
+        end
+        yield text
+      end
+    end
+  end
+
+  # takes a astring and pushes a cell text into it, with required padding
+  # to make each cell of given column same size
+  def self.push_cell_into! string, text, cell_size
+    padd = cell_size - text.length
+    string << text
+    string << (" " * padd) if padd > 0
+  end
+end
